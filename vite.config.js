09eb760120d50fc8278e3d91ddbedd354447de33
othmanalmasri <engothmanalmasri@gmail.com>
2025-05-26 18:49:05 +0300

    Edit config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/simple-3D-/",
   plugins: [react(), tailwindcss()],
+  base: "/simple-3D-",
 });
